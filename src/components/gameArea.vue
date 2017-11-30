276928f4d5a366f529d5a10b5c10f007ba2c62aa
ConorHawk <conornhawkins@gmail.com>
2017-11-30 22:29:11 +1300

    started project

---
+++ src/components/gameArea.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+  <div class="game-area">
+    <slot></slot>
+    <slot name="sidebar"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "game-area"
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ src/components/sidebar.vue
@@ -0,0 +1,11 @@
+<template lang="html">
+  <p>Test2</p>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
